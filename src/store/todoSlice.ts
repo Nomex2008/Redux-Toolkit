550bdc0cda0806965d2f47e8a1623d3c32a75dce
Nomex2008 <97978327+Nomex2008@users.noreply.github.com>
2024-09-22 23:57:54 +0300

    Commit 1

---
+++ src/store/todoSlice.ts
@@ -0,0 +1,35 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { type } from 'os';
+
+type Todo = {
+    id: string;
+    title: string;
+    completed: boolean;
+}
+
+const todoSlice = createSlice({
+    name: 'todos',
+    initialState: {
+        todos: [],
+    },
+    reducers: {
+        addTodo(state, action) {
+            state.todos.push({
+              id: new Date().toISOString(),
+              title: action.payload.text,
+              completed: false,
+            });
+        },
+        toggleComplete(state, action) {
+            const toggledTodo = state.todos.find(todo => todo.id === action.payload.id);
+            toggledTodo.completed = !toggledTodo.completed;
+        },
+        removeTodo(state, action) {
+            state.todos = state.todos.filter(todo => todo.id !== action.payload.id);
+        }
+    },
+});
+
+export const {addTodo, toggleComplete, removeTodo} = todoSlice.actions;
+
+export default todoSlice.reducer;
